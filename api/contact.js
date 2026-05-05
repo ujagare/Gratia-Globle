5e6a6f4a032cd21b1aa317a78d57b7157794f0a3
DD Tech <Info@ddtech.in>
2026-05-05 15:27:30 +0530

    Add CORS headers to API endpoint

--- api/contact.js
+++ api/contact.js
@@ -2,6 +2,15 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 module.exports = async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
